4733b0105f5879e09629ae2a1b44d366742b97fa
sabriyeuregen <sabriyeuregen@gmail.com>
2023-02-14 20:26:05 +0300

    Added : button on card

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -6,11 +6,19 @@ type ProductProps = {
 }
 
 const ProductCard = ({id,name,price,imgUrl}:ProductProps) => {
+
+ const quantity = 0;
+
   return (
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow  dark:bg-gray-800 dark:border-gray-700">
        <div className="rounded-t-lg"><img src={imgUrl} height="200" style={{objectFit: "cover"}}></img></div>
        <div className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</div>
        <div className="mb-3 font-normal text-gray-700 dark:text-gray-400">{price}</div>
+       <div className="">
+        {
+          quantity === 0 ? <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">+Add To Cart</button> : <div className="" style={{gap:".5rem"}}></div>
+        }
+       </div>
     </div>
   )
 }
