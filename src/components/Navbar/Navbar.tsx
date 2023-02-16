77e74d0966a995261a2033284c5f2ef6f8ce56f9
sabriyeuregen <sabriyeuregen@gmail.com>
2023-02-16 15:08:51 +0300

    Added : ShoppingCart

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,9 @@
 import { Link } from "react-router-dom";
+import { useShoppingCart } from "../../store/shoppingcart-contex";
+import ShoppingCart from "../ShoppingCart/ShoppingCart";
 const Navbar = () => {
+  const { openCart, cartQuantity } = useShoppingCart();
+
   return (
     <div className="flex items-center">
       <Link
@@ -21,25 +25,30 @@ const Navbar = () => {
         Hakkımızda
       </Link>
       <div className="hidden xl:flex items-center space-x-5 items-center rounded-full">
-        <button>
-          <a className="hover:text-gray-200" href="#">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-              />
-            </svg>
-            <div className="flex justify-center items-center rounded-full bg-purple-500 absolute bottom-0 w-6 h-6 right-5">3</div>
-          </a>
-        </button>
+        {cartQuantity > 0 &&  (
+          <button onClick={openCart}>
+            <a className="hover:text-gray-200" href="#">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                />
+              </svg>
+
+              <div className="flex justify-center items-center rounded-full bg-purple-500 absolute bottom-0 w-6 h-6 right-5">
+                {cartQuantity}
+              </div>
+            </a>
+          </button>
+        )}
       </div>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-
+import { ShoppingCartProvider } from './store/shoppingcart-contex'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <ShoppingCartProvider>
+    <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </ShoppingCartProvider>
+  
 )
